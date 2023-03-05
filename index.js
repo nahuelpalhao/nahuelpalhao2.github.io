0f83d43e92bda07b22a72b268556e3924bc2ead3
nahuelpalhao <123119323+nahuelpalhao@users.noreply.github.com>
2023-03-05 17:49:38 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+document. getElementById ("boton_color1") . addEventListener ("click" , function()  {
+   
+    document.body.style.backgroundColor = "yellow"
+}  ) ;
+document. getElementById ("boton_color2") . addEventListener ("click" , function()  {
+   
+    document.body.style.backgroundColor = "grey"
+}  ) ;
+
+
+
+
+document.getElementById("es").addEventListener("click", function() {
+  idioma = "es";
+  
+} );
+
+
+document.getElementById("en").addEventListener("click", function() {
+  idioma = alert( "algo salio mal busca en la consola /something went wrong search the console" );
+  
+});
+
+
+document. getElementById ("default") . addEventListener ("click" , function()  {
+   
+  document.body.style.backgroundColor = "white"
+}  ) ;
+
+
